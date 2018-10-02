9fea09260adcba6a20f4905a3dffc044bba97687
bakutkisa241 <35742714+bakutkisa241@users.noreply.github.com>
2018-10-02 20:20:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,104 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
 var prefix = "#";
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const moment = require('moment');
+const ytdl = require('ytdl-core');
+const request = require('request');
+var Canvas = require('canvas')
+const prettyMs = require('pretty-ms');
+const fkkRecently = new Set();
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const fs = require('fs');
+const ms = require("ms");
+const dateFormat = require('dateformat');
+const config = require("./config.json")
+
+
+var user = {};
+var warn = {};
+
+
+
+client.on('message', function(message) {
+
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 4) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 4) {		   
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+                   Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+    
+     var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+      delete warn[message.author.id];
+      delete user[message.author.id];
+      const embed500 = new Discord.RichEmbed()
+       .setTitle(`المرسل ${message.author.username}#${message.author.discriminator} `)
+        .setDescription(":white_check_mark:  | `محاولة السبام`\n\nالاسم:\n"+`${message.author.username}#${message.author.discriminator}`+"\nالعقوبة:\n  MuteChat / ميوت كتابي\n")
+        .setFooter("Anti - Spam")
+        .setColor("#c91616")
+      message.channel.send(embed500)
+          const embed20 = new Discord.RichEmbed()
+        .setTitle(":scales: | تمت معاقبتك")
+        .setDescription(`**:small_blue_diamond:لقد قمت بمخالفة قوانين السيرفر**\n \n:gun: : نوع العقوبه\nMuteChat / ميوت كتابي\n:clock1: وقت وتاريخ العقوبه:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`في حال كانت العقوبة بالغلط, تواصل مع الادارة`")
+            .setFooter("Anti - Spam")
+        .setColor("#c91616")
+      
+       message.author.send(embed20)
+    
+    }
+  });
+
 
 
 
